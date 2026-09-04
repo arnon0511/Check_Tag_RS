6d702aa79a494053455c6f9ebee37af530c02f6a
arnon0511 <Arnon.ju@tskforging.com>
2026-09-05 05:15:08 +0700

    Add InputType import to MainActivity.kt

--- app/src/main/java/com/tskforging/checktagrs/MainActivity.kt
+++ app/src/main/java/com/tskforging/checktagrs/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.ClipData
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.text.InputType
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.*
